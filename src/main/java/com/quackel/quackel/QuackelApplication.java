4393e89a9499236248ab72b339df5f803cae22e3
Fredrik Lindström <fredrik_lindstrom@live.com>
2021-11-23 13:09:18 +0100

    Initial commit

---
+++ src/main/java/com/quackel/quackel/QuackelApplication.java
@@ -0,0 +1,13 @@
+package com.quackel.quackel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuackelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuackelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quackel/quackel/QuackelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quackel.quackel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuackelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
