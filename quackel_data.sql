d65a72577c244db00fa2dd2990c97f8b361ee4b0
Fabian Ekbäck <fabian.ekback@gmail.com>
2021-11-24 15:26:25 +0100

    Schema och data till Quackeldb

---
+++ quackel_data.sql
@@ -0,0 +1,30 @@
+USE Quackeldb;
+
+INSERT INTO user(userName)
+VALUES
+("shaquille.oatmeal"),
+("hanging_with_my_gnomies"),
+("fast_and_the_curious"),
+("averagestudent"),
+("google_was_my_idea"),
+("real_name_hidden"),
+("HairyPoppins"),
+("Joe Not Exotic");
+
+INSERT INTO message(userID, body)
+VALUES
+(1001, "Up unpacked friendly ecstatic so possible humoured do. Ample end might folly quiet one set spoke her."),
+(1001, "We no am former valley assure. Four need spot ye said we find mile. Are commanded him convinced dashwoods did estimable forfeited."),
+(1002, "Shy celebrated met sentiments she reasonably but. Proposal its disposed eat advanced marriage sociable."),
+(1002, "Drawings led greatest add subjects endeavor gay remember."),
+(1002, "Principles one yet assistance you met impossible"),
+(1003, "Resources exquisite set arranging moonlight sex him household had."),
+(1004, "Months had too ham cousin remove far spirit. She procuring the why performed continual improving."),
+(1004, "Civil songs so large shade in cause. Lady an mr here must neat sold."),
+(1005, "Children greatest ye extended delicate of."),
+(1005, "No elderly passage earnest as in removed winding or."),
+(1006, "Ever man are put down his very. And marry may table him avoid."),
+(1007, "Hard sell it were into it upon. He forbade affixed parties of assured to me windows."),
+(1007, "Happiness him nor she disposing provision. Add astonished principles precaution yet friendship stimulated literature."),
+(1008, "State thing might stand one his plate. Offending or extremity therefore so difficult he on provision."),
+(1008, "Tended depart turned not are.");
