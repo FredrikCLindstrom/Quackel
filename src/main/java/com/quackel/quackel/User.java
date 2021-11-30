62c5f8c6e798d67861657d55c42e3686d87ea288
Fabian Ekbäck <fabian.ekback@gmail.com>
2021-11-30 14:30:23 +0100

    Skapade koppling mellan databas och UI för att skapa nya användare

--- src/main/java/com/quackel/quackel/User.java
+++ src/main/java/com/quackel/quackel/User.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +14,9 @@ import java.util.List;
 @ToString
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
--- src/main/java/com/quackel/quackel/UserController.java
+++ src/main/java/com/quackel/quackel/UserController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.websocket.server.PathParam;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Controller//tog bort restController
 @RequestMapping("/api")
@@ -69,7 +71,22 @@ public class UserController {
         model.addAttribute("quack", quack);
 
         return "quackbyid.html";
+    }
 
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerNewUser() {
+        return "register.html";
+    }
+
+    @RequestMapping(value ="/saveUser", method =RequestMethod.POST)
+    public ModelAndView save(@ModelAttribute User user) {
+        user.setName(user.getName());
+        userService.registerNewUser(user);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new_user_info.html");
+        modelAndView.addObject("user", user);
+        return modelAndView;
     }
 }
 
